b78ef15af54ec3a043f3a0fa6b8ea3d87383fc27
MasudHero <masudrana2003galiboy@gmail.com>
2023-10-02 13:52:17 +0600

    javascript practic

--- hello.js
+++ hello.js
@@ -10,23 +10,45 @@
 // var name = "My name is Masud Rana";
 // console.log(typeof name);
 // var promise ="My name is Masud Rana and My father's name is Jamal uddin and My Mother Name is Tara banu and My Village Name is Matar Para and I have one brother and one sisther and I am wev developer and javascript and php laravel database developer and I am a happy but now i have some problem....";
-// console.log(promise .toUpperCase());
+// // console.log(promise .toUpperCase());
 
 
-var shirtPrice = 20;
-if(shirtPrice != 30){
-    console.log("shirt is need");
-}
-else{
-    console.log("shirt is not need");
-}
+// var shirtPrice = 20;
+// if(shirtPrice != 30){
+//     console.log("shirt is need");
+// }
+// else{
+//     console.log("shirt is not need");
+// }
 
 
-var jobKores = true;
-var amount = 10000;
-if(jobKores == true && amount < 50000){
-    console.log("tahole toke motor saikel kine dibo");
-}
-else{
-    console.log("toke motorsaikel kine dibo na");
-}
+// var jobPaico = true;
+// var amount = 6000;
+// if(jobPaico == true && amount < 2000){
+//     console.log("tumake car cine dibo")
+// }
+// else if(jobPaico == true){
+//     console.log("tor kopule car nai");
+// }
+// else{
+//     console.log("tumake car kine dibo na masud");
+// }
+
+
+// var studentName = ["masud","Himel","shanto","shakil","Jibon","Anower","Ruman","Arif"];
+// var masud = studentName [4];
+// console.log(masud);
+// console.log(studentName [4]);
+// var totul = studentName.indexOf("bablu Mia");
+// console.log(totul);
+// var studentAge = [59,33,44,22,33,44,55,66,77,655,44,33,22,44,5,65,76,554,33,32];
+// console.log(studentAge);
+// var result = studentAge.indexOf(32);
+// console.log(result);
+// var allFriends = [11,22,33,44,55,66,77,88,99];
+
+// var totul = allFriends [3];
+// console.log(totul);
+var allName = ["Masud","Rana","bijoy","Anower","Himel","Shakil","Roman","momin","Ashan","shamiul"];
+allName.pop("Arif");
+console.log(allName.length);
